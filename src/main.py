fb6ba5119aa3461a6dfaead0c881731b46e9ffbe
Matejop <matej.fibiger@gmail.com>
2026-04-22 20:49:11 +0200

    Backbrop optimized. Code structure cleaned up. Refactored mnist, y is now a vector. Validation implemented. Exception raising implemented except in trainer.py

--- src/main.py
+++ src/main.py
@@ -1,10 +1,28 @@
-from data_manager import DataManager
-from objects.model import Model
+from trainer import Trainer
+from model import Model
+import orjson
+import gzip
+import os
+
+#TODO add filepath to global config
+DATA_PATH = os.path.join("data", "mnist_preprocessed_vector.gz")
 
 if __name__ == "__main__":
-    data = DataManager.load_data()
-    training, validation, testing = data
-    #model = Model.fit(data, [784, 64, 10])
-    model = Model()
-    model.load("C:\\Users\\matej\\source\\MLITG\\src\\objects\\../../models\\94c25e0e32140178575781e497016225.json")
-    print(model.infer_dataset(testing))
+    path = "c:/Users/matej/source/MLITG/data/mnist_preprocessed_vector.gz"
+    data = orjson.loads(gzip.open(path).read())
+    trainer = Trainer(
+        max_depth=3, 
+        layer_size_start=256, 
+        layer_size_end=300, 
+        epochs_start=3, 
+        epochs_end=3, 
+        batch_size_start=1000, 
+        batch_size_end=2000,
+        learning_rate_start=0.005,
+        learning_rate_end=0.02,
+        activation="relu"
+    )
+    model = trainer.random_search(data["training_x"], data["training_y"], data["validation_x"], data["validation_y"])
+    #model = Model([len(data["training_x"][0]), 256, len(data["training_y"][0])], "relu", 3, 1000, 0.01)
+    #model.train(data["training_x"], data["training_y"])
+    print(model.measure_accuracy(data["testing_x"], data["testing_y"]))
